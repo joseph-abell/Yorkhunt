a79ccf717726ec63432f8c97c51e562633e20f8f
Joe Abell <joe.abell@isotoma.com>
2020-10-08 09:03:43 +0100

    form

---
+++ src/pages/contribute-success.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Layout from '../components/Layout'
+
+const ContributeSuccess = () => (
+  <Layout>
+    <main>
+      <section className="section">
+        <div className="container">
+          <div className='content'>
+            <h2 className='subtitle'>Success</h2>
+            <p>Thank you gfor your contribution!</p>
+          </div>
+        </div>
+      </section>
+    </main>
+  </Layout>
+)
+
+export default ContributeSuccess;
--- src/pages/contribute.js
+++ src/pages/contribute.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { navigate } from 'gatsby-link'
 import Layout from '../components/Layout'
 
-const Contribute = () => (
+function encode(data) {
+    return Object.keys(data)
+      .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+      .join('&')
+  }
+
+const Contribute = () => {
+  const [formState, setFormState] = useState({});
+
+  const handleChange = (e) => {
+      setFormState({ ...formState, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const form = e.target
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...formState,
+      }),
+    })
+      .then(() => navigate(form.getAttribute('action')))
+      .catch((error) => alert(error))
+  }
+
+  return (
   <Layout>
     <main>
       <section className="section">
@@ -14,13 +43,22 @@ const Contribute = () => (
               method="post"
               data-netlify="true"
               data-netlify-honeypot="bot-field"
+              action='/contribute-success/'
+              onSubmit={handleSubmit}
             >
               <input type="hidden" name="form-name" value="suggest-map" />
+
+              <p hidden>
+                <label htmlFor='botField'>
+                  Don’t fill this out:
+                </label>
+                <input name="bot-field" id='botField' onChange={handleChange} />
+              </p>
             
               <div>
                 <p>
                   <label htmlFor='name'>Name: * (Used for credits)</label>
-                  <input id='name' type='text' style={{ borderRadius: '4px', border: '1px solid #bbb', padding: '7px' }} />
+                  <input id='name' type='text' style={{ borderRadius: '4px', border: '1px solid #bbb', padding: '7px' }} onChange={handleChange} />
                 </p>
               </div>
 
@@ -28,14 +66,14 @@ const Contribute = () => (
                 <p>
                   <label htmlFor='contribution'>Contribution: *</label>
                 
-                  <textarea id='contribution' style={{ minWidth: '245px', minHeight: '100px', borderRadius: '4px', border: '1px solid #bbb' }} />
+                  <textarea id='contribution' style={{ minWidth: '245px', minHeight: '100px', borderRadius: '4px', border: '1px solid #bbb' }} onChange={handleChange} />
                 </p>
               </div>
 
               <div>
                 <p>
                   <label htmlFor='email'>Email: * (Used to let you know when your contribution has made it on to the site)</label>
-                  <input id='email' type='email' style={{ borderRadius: '4px', border: '1px solid #bbb', padding: '7px' }} />
+                  <input id='email' type='email' style={{ borderRadius: '4px', border: '1px solid #bbb', padding: '7px' }} onChange={handleChange} />
                 </p>
               </div>
 
@@ -48,6 +86,6 @@ const Contribute = () => (
       </section>
     </main>
   </Layout>
-)
+)}
 
 export default Contribute;
